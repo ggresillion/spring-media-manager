f24c4bc2a05e41e436963e84059477edbf957a30
GRESILLION Guillaume <guillaume.gresillion@thalesgroup.com>
2020-02-12 18:32:55 +0100

    refacto architecture

---
+++ src/main/java/com/ggresillion/spring/mediamanager/configurations/ConditionalConfiguration.java
@@ -0,0 +1,9 @@
+package com.ggresillion.spring.mediamanager.configurations;
+
+import com.ggresillion.spring.mediamanager.models.MediaEntity;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConditionalConfiguration {
+}
